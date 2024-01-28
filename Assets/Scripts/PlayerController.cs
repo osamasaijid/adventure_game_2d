1ad59b553f1a3f91f56960eefd98094b1167be53
Osama Sajid <osamasaijidk@gmail.com>
2024-01-28 23:54:46 +0500

    implementing the basic player movement and rotation, added enemy detection and pulling mechanics. Also added the mana regeneration.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float attackRange = 1f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 20;
 
     private Rigidbody2D rb;
     private Vector2 movement;
-    private Vector2 mousePosition;
+
+    public float hookRange = 5f;
+    public float pullSpeed = 5f;
+    private GameObject hookedEnemy = null;
+
+    public int maxMana = 100;
+    public int hookManaCost = 20;
+    public float manaRegenerationRate = 5f;
+    private int currentMana;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentMana = maxMana;
     }
 
     void Update()
@@ -24,11 +36,82 @@ public class PlayerController : MonoBehaviour
         {
             transform.up = movement;
         }
+
+        // Melee attack
+        if (Input.GetKeyDown(KeyCode.Space)) // Assuming space bar is the attack key
+        {
+            Attack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E)) // Assuming E is the hook key
+        {
+            TryHook();
+        }
+
+        if (hookedEnemy != null)
+        {
+            PullEnemy();
+        }
+        // Regenerate Mana
+        if (currentMana < maxMana)
+        {
+            currentMana += (int)(manaRegenerationRate * Time.deltaTime);
+            currentMana = Mathf.Min(currentMana, maxMana);
+        }
     }
 
     void FixedUpdate()
     {
         // Move the player
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Attack()
+    {
+        // Detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, attackRange, enemyLayers);
+
+        // Damage them
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+            // Here, you can add code to damage the enemy
+            // For example, enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+    void TryHook()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, hookRange, enemyLayers);
+        if (hit.collider != null)
+        {
+            hookedEnemy = hit.collider.gameObject;
+            Debug.Log("Hooked " + hookedEnemy.name);
+
+            // Consume Mana
+            currentMana -= hookManaCost;
+        }
+    }
+
+    void PullEnemy()
+    {
+        // Pull the enemy towards the player
+        hookedEnemy.transform.position = Vector2.MoveTowards(hookedEnemy.transform.position, transform.position, pullSpeed * Time.deltaTime);
+
+        // Release the enemy if it gets close enough to the player
+        if (Vector2.Distance(transform.position, hookedEnemy.transform.position) < 1f)
+        {
+            hookedEnemy = null;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Existing Gizmos for attack range...
+        //    // Display the attack range in editor
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+
+        // Display the hook range in editor
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + transform.up * hookRange);
     }
 }
